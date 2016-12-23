2a5afb1c4bc1aa11718161433fe7410e13904d72
Roman Svarichevskiy <n.e.w_w.a.v.e@mail.ru>
2016-12-23 10:24:09 +0300

    qq

---
+++ src/test/resources/test.sql
@@ -0,0 +1,5 @@
+#insert into users( name, age, email) values('Alex', 22, 'alex@devcolibri.com');
+#select  * from users ;
+#select name, age from users where name = 'Steve' ;
+#update users set name='Steve', age=55 where id=1;
+#delete from users;
