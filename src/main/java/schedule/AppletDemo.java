8ab79709d02564768c9c0064ff1b3f31a02972cd
Roman Svarichevskiy <n.e.w_w.a.v.e@mail.ru>
2016-12-26 20:56:16 +0300

    appletstart.class

---
+++ src/main/java/schedule/AppletDemo.java
@@ -0,0 +1,14 @@
+package schedule;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by Роман on 26.12.2016.
+ */
+public class AppletDemo  extends Applet{
+    public void paint(Graphics g){
+        g.drawString("Simple applet", 20, 20);
+    }
+
+}
